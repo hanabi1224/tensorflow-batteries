dceb966127bc583416649ba874daf9c5521a20da
hanabi1224 <harlowmoo@gmail.com>
2018-07-15 00:56:32 -0700

    Winx64 (#1)

---
+++ src/TensorflowBinariesBuildTask/TensowflowBinariesBuildTask.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace TensorflowBinariesBuildTask
+{
+    public class TensowflowBinariesBuildTask : Microsoft.Build.Utilities.Task
+    {
+        private const string PypiUrlTemplate = @"https://pypi.org/project/{0}/{1}/#files";
+
+        public string PypiPackageName { get; set; }
+
+        public string PypiPackageVersion { get; set; }
+
+        public string Runtime { get; set; }
+
+        public string OutputPath { get; set; }
+
+        public override bool Execute()
+        {
+            return ExecuteAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        private async Task<bool> ExecuteAsync()
+        {
+            var url = string.Format(PypiUrlTemplate, PypiPackageName, PypiPackageVersion);
+            using (var httpClient = new HttpClient())
+            {
+                var stream = await httpClient.GetStreamAsync(url).ConfigureAwait(false);
+                var htmlDoc = new HtmlDocument();
+                htmlDoc.Load(stream);
+                var anchors = htmlDoc.DocumentNode.SelectNodes(@".//div[@id='files']//td//a");
+                var selection = anchors.FirstOrDefault(_ => _.InnerText.ToLowerInvariant().Contains(Runtime.ToLowerInvariant()));
+                var wheelLink = selection.GetAttributeValue("href", null);
+
+                using (var ms = new MemoryStream(await httpClient.GetByteArrayAsync(wheelLink)))
+                using (var zip = new ZipArchive(ms))
+                {
+                    foreach (var entry in zip.Entries.Where(_ => _.FullName.EndsWith(".pyd")))
+                    {
+                        var dir = Path.GetDirectoryName(OutputPath);
+                        if (!Directory.Exists(dir))
+                        {
+                            Directory.CreateDirectory(dir);
+                        }
+
+                        entry.ExtractToFile(OutputPath, true);
+                        break;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ test/TensorflowBinariesBuildTask.Tests/BuildTaskTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace TensorflowBinariesBuildTask.Tests
+{
+    [Parallelizable(ParallelScope.All)]
+    [TestFixture]
+    public class BuildTaskTests
+    {
+        [Test]
+        [TestCase("tensorflow", "1.2.0")]
+        [TestCase("tensorflow", "1.8.0")]
+        [TestCase("tensorflow", "1.9.0")]
+        [TestCase("tensorflow-gpu", "1.2.0")]
+        [TestCase("tensorflow-gpu", "1.8.0")]
+        [TestCase("tensorflow-gpu", "1.9.0")]
+        public void WindowsX64(string packageName, string packageVersion)
+        {
+            var task = new TensowflowBinariesBuildTask
+            {
+                OutputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, packageName, packageVersion, "libtensorflow.dll"),
+                PypiPackageName = packageName,
+                PypiPackageVersion = packageVersion,
+                Runtime = "win",
+            };
+
+            task.Execute();
+
+            FileAssert.Exists(task.OutputPath);
+        }
+    }
+}
