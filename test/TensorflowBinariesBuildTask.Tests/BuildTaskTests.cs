c8be6df75542d03e90856a117df37d8a607bad4b
hanabi1224 <hailmu@microsoft.com>
2018-07-20 16:21:20 -0700

    fix tests for linux

--- test/TensorflowBinariesBuildTask.Tests/BuildTaskTests.cs
+++ test/TensorflowBinariesBuildTask.Tests/BuildTaskTests.cs
@@ -9,18 +9,71 @@ using TensorflowBinariesBuildTask.Core;
 
 namespace TensorflowBinariesBuildTask.Tests
 {
+    public static class NativeMethods
+    {
+        private const int RTLD_NOW = 2;
+
+        public static IntPtr LoadLibrary(string lpFileName)
+        {
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.Unix:
+                    return LoadLibraryLinux(lpFileName, RTLD_NOW);
+                default:
+                    return LoadLibraryWindows(lpFileName);
+            }
+        }
+
+        public static bool FreeLibrary(IntPtr hModule)
+        {
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.Unix:
+                    return FreeLibraryLinux(hModule) == 0;
+                default:
+                    return FreeLibraryWindows(hModule);
+            }
+        }
+
+        public static IntPtr GetProcAddress(IntPtr hModule, string procedureName)
+        {
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.Unix:
+                    return GetProcAddressLinux(hModule, procedureName);
+                default:
+                    return GetProcAddressWindows(hModule, procedureName);
+            }
+        }
+
+        // http://dimitry-i.blogspot.com/2013/01/mononet-how-to-dynamically-load-native.html
+        [DllImport("kernel32", EntryPoint = "LoadLibrary")]
+        private static extern IntPtr LoadLibraryWindows(string lpFileName);
+
+        [DllImport("kernel32", EntryPoint = "FreeLibrary", SetLastError = true)]
+        private static extern bool FreeLibraryWindows(IntPtr hModule);
+
+        [DllImport("kernel32", EntryPoint = "GetProcAddress")]
+        private static extern IntPtr GetProcAddressWindows(IntPtr hModule, string procedureName);
+
+        [DllImport("libdl", EntryPoint = "dlopen")]
+        private static extern IntPtr LoadLibraryLinux(String fileName, int flags);
+
+        [DllImport("libdl", EntryPoint = "dlsym")]
+        private static extern IntPtr GetProcAddressLinux(IntPtr handle, String symbol);
+
+        [DllImport("libdl", EntryPoint = "dlclose")]
+        private static extern int FreeLibraryLinux(IntPtr handle);
+
+        [DllImport("libdl", EntryPoint = "dlerror")]
+        private static extern IntPtr dlerror();
+    }
+
     [Parallelizable(ParallelScope.All)]
     [TestFixture]
     public class BuildTaskTests
     {
-        [DllImport("kernel32")]
-        static extern IntPtr LoadLibrary(string lpFileName);
 
-        [DllImport("kernel32", SetLastError = true)]
-        static extern bool FreeLibrary(IntPtr hModule);
-
-        [DllImport("kernel32.dll")]
-        public static extern IntPtr GetProcAddress(IntPtr hModule, string procedureName);
 
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         private delegate IntPtr TF_Version();
@@ -71,14 +124,14 @@ namespace TensorflowBinariesBuildTask.Tests
 
             if (!shouldSkipTesting)
             {
-                var pLib = LoadLibrary(libFullPath);
-                var pFunc = Marshal.GetDelegateForFunctionPointer<TF_Version>(GetProcAddress(pLib, nameof(TF_Version)));
+                var pLib = NativeMethods.LoadLibrary(libFullPath);
+                var pFunc = Marshal.GetDelegateForFunctionPointer<TF_Version>(NativeMethods.GetProcAddress(pLib, nameof(TF_Version)));
 
                 var versionPtr = pFunc();
                 var version = Marshal.PtrToStringAnsi(versionPtr);
                 version.Should().Contain(packageVersion);
 
-                FreeLibrary(pLib);
+                NativeMethods.FreeLibrary(pLib);
             }
         }
 
