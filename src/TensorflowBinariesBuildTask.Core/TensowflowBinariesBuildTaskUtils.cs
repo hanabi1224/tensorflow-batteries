ba04a18113a3159bd340c8844358b05bfd6e3875
hanabi1224 <hailmu@microsoft.com>
2018-07-20 14:07:53 -0700

    dll validation tests

---
+++ src/TensorflowBinariesBuildTask.Core/TensowflowBinariesBuildTaskUtils.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace TensorflowBinariesBuildTask.Core
+{
+    public static class TensowflowBinariesBuildTaskUtils
+    {
+        private const string PypiUrlTemplate = @"https://pypi.org/project/{0}/{1}/#files";
+
+        public static async Task<bool> ExecuteAsync(
+            string runtime,
+            string pythonVersion,
+            string pypiPackageName,
+            string pypiPackageVersion,
+            string outputPath)
+        {
+            var url = string.Format(PypiUrlTemplate, pypiPackageName, pypiPackageVersion);
+            using (var httpClient = new HttpClient())
+            {
+                var stream = await httpClient.GetStreamAsync(url).ConfigureAwait(false);
+                var htmlDoc = new HtmlDocument();
+                htmlDoc.Load(stream);
+                var anchors = htmlDoc.DocumentNode.SelectNodes(@".//div[@id='files']//td//a");
+                var selection = anchors
+                    .Where(_ => _.InnerText.ToLowerInvariant().Contains(pythonVersion))
+                    .First(_ => _.InnerText.ToLowerInvariant().Contains(runtime.ToLowerInvariant()));
+
+                var wheelLink = selection.GetAttributeValue("href", null);
+
+                using (var ms = new MemoryStream(await httpClient.GetByteArrayAsync(wheelLink)))
+                using (var zip = new ZipArchive(ms))
+                {
+                    foreach (var entry in zip.Entries.Where(_ => _.FullName.EndsWith(".pyd")))
+                    {
+                        var dir = Path.GetDirectoryName(outputPath);
+                        if (!Directory.Exists(dir))
+                        {
+                            Directory.CreateDirectory(dir);
+                        }
+
+                        entry.ExtractToFile(outputPath, true);
+                        break;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- test/TensorflowBinariesBuildTask.Tests/BuildTaskTests.cs
+++ test/TensorflowBinariesBuildTask.Tests/BuildTaskTests.cs
@@ -1,33 +1,90 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using FluentAssertions;
 using NUnit.Framework;
+using TensorflowBinariesBuildTask.Core;
 
 namespace TensorflowBinariesBuildTask.Tests
 {
-    [Parallelizable(ParallelScope.All)]
+    //[Parallelizable(ParallelScope.All)]
+    [NonParallelizable]
     [TestFixture]
     public class BuildTaskTests
     {
+        [DllImport("kernel32")]
+        static extern IntPtr LoadLibrary(string lpFileName);
+
+        [DllImport("kernel32", SetLastError = true)]
+        static extern bool FreeLibrary(IntPtr hModule);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetProcAddress(IntPtr hModule, string procedureName);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate IntPtr TF_Version();
+
         [Test]
-        [TestCase("tensorflow", "1.2.0")]
-        [TestCase("tensorflow", "1.8.0")]
-        [TestCase("tensorflow", "1.9.0")]
-        [TestCase("tensorflow-gpu", "1.2.0")]
-        [TestCase("tensorflow-gpu", "1.8.0")]
-        [TestCase("tensorflow-gpu", "1.9.0")]
-        public void WindowsX64(string packageName, string packageVersion)
+        [TestCaseSource(nameof(CreateTestCases))]
+        public async Task E2EAsync(
+            string runtime,
+            string packageName,
+            string packageVersion,
+            bool shouldSkipTesting)
         {
-            var task = new TensowflowBinariesBuildTask
+            Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            var outputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, packageName, packageVersion, "libtensorflow.dll");
+            const string pythonVersion = "cp36";
+
+            await TensowflowBinariesBuildTaskUtils.ExecuteAsync(
+                runtime: runtime,
+                pythonVersion: pythonVersion,
+                pypiPackageName: packageName,
+                pypiPackageVersion: packageVersion,
+                outputPath: outputPath);
+
+            FileAssert.Exists(outputPath);
+
+            if (!shouldSkipTesting)
             {
-                OutputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, packageName, packageVersion, "libtensorflow.dll"),
-                PypiPackageName = packageName,
-                PypiPackageVersion = packageVersion,
-                Runtime = "win",
-            };
+                var pLib = LoadLibrary(outputPath);
+                var pFunc = Marshal.GetDelegateForFunctionPointer<TF_Version>(GetProcAddress(pLib, nameof(TF_Version)));
+                
+                var versionPtr = pFunc();
+                var version = Marshal.PtrToStringAnsi(versionPtr);
+                version.Should().Contain(packageVersion);
 
-            task.Execute();
+                FreeLibrary(pLib);
+            }           
+        }
 
-            FileAssert.Exists(task.OutputPath);
+        public static IEnumerable<TestCaseData> CreateTestCases()
+        {
+            string runtime;
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.MacOSX:
+                    runtime = "osx";
+                    break;
+                case PlatformID.Unix:
+                    runtime = "linux";
+                    break;
+                default:
+                    runtime = "win";
+                    break;
+            }
+
+            foreach (var package in new[] { "tensorflow", "tensorflow-gpu" })
+            {
+                var shouldSkipTesting = package.Contains("gpu");
+                foreach (var version in new[] { "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8", "1.9" })
+                {
+                    yield return new TestCaseData(runtime, package, version, shouldSkipTesting).SetName($"[{runtime}][{package}][{version}]");
+                }
+            }
         }
     }
 }
