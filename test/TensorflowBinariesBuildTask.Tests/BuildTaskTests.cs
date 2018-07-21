bfe086166980ee71f77b7111b0403a54450df71e
hanabi1224 <hailmu@microsoft.com>
2018-07-20 19:11:34 -0700

    test

--- test/TensorflowBinariesBuildTask.Tests/BuildTaskTests.cs
+++ test/TensorflowBinariesBuildTask.Tests/BuildTaskTests.cs
@@ -120,8 +120,8 @@ namespace TensorflowBinariesBuildTask.Tests
                 var shouldSkipTesting = package.Contains("gpu");
                 foreach (var version in new[] 
                 {
-                    "1.2", "1.2.1", "1.3", "1.4", "1.5", "1.5.1", "1.6", "1.7", "1.7.1",
-                    "1.8",
+                    //"1.2", "1.2.1", "1.3", "1.4", "1.5", "1.5.1", "1.6", "1.7", "1.7.1",
+                    //"1.8",
                     "1.9",
                 })
                 {
