860990635147b7117c00ed319c8b6a0ae489f2bd
hanabi1224 <harlowmoo@gmail.com>
2018-07-21 12:57:18 -0700

    linux nuget

---
+++ src/TensorflowBinariesBuildTask/TensowflowBinariesBuildFromStorageTask.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using TensorflowBinariesBuildTask.Core;
+
+namespace TensorflowBinariesBuildTask
+{
+    public class TensowflowBinariesBuildFromStorageTask : Microsoft.Build.Utilities.Task
+    {
+        public string OS { get; set; }
+
+        public string Device { get; set; }
+
+        public string Version { get; set; }
+
+        public string OutputDir { get; set; }
+
+        public override bool Execute()
+        {
+            return ExecuteAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        private Task<bool> ExecuteAsync()
+        {
+            return TensowflowBinariesBuildTaskUtils.BuildFromStorageAsync(
+                os: OS,
+                device: Device,
+                version: Version,
+                outputDir: OutputDir);
+        }
+    }
+}
